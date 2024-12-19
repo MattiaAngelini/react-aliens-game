d0de86848a50668fc8bac236cfcdc2611fc99a1c
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-12-19 23:58:48 +0100

    Added characters appearance handling based on user selection

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,16 +1,90 @@
 import HeroImage from './assets/images/hero.jpg'
+import flareonBack from './assets/images/flareon_back.jpg'
+import flareonFront from './assets/images/flareon_front.jpg'
+
+import glacioraBack from './assets/images/glaciora_back.jpg';
+import glacioraFront from './assets/images/glaciora_front.jpg';
+
+import meteoraxBack from './assets/images/meteorax_back.jpg';
+import meteoraxFront from './assets/images/meteorax_front.jpg';
+
+import voidonBack from './assets/images/voidon_back.jpg';
+import voidonFront from './assets/images/voidon_front.jpg';
+
 import './assets/styles/Game.scss'
 import { useSelector } from 'react-redux';
 
 function Game() {
-  console.log(useSelector((state) => state.players.player1))
-  console.log(useSelector((state) => state.players.player2))
+  const player1Choice = useSelector((state) => state.players.player1)
+  const player2Choice = useSelector((state) => state.players.player2)
+ 
+  const aliensAvatar = [
+    {
+      id: 1,
+      name: "Flareon",
+      imgFront: flareonFront,
+      imgBack: flareonBack
+    },
+    {
+      id: 2,
+      name: "Glaciora",
+      imgFront: glacioraFront,
+      imgBack: glacioraBack
+    },
+    {
+      id: 3,
+      name: "Meteorax",
+      imgFront: meteoraxFront,
+      imgBack: meteoraxBack
+    },
+    {
+      id: 4,
+      name: "Voidon",
+      imgFront: voidonFront,
+      imgBack: voidonBack
+    }
+  ];
+  
+  
+  function renderAlienBack(){
+    for(let i = 0; i < aliensAvatar.length; i++){
+          if(aliensAvatar[i].name === player1Choice){
+            return aliensAvatar[i].imgBack
+          }
+    }
+  }
+  function renderAlienFront(){
+    for(let i = 0; i < aliensAvatar.length; i++){
+          if(aliensAvatar[i].name === player2Choice){
+            return aliensAvatar[i].imgFront
+          }
+    }
+  }
+
+  console.log(renderAlienBack(), renderAlienFront());
+
   return (
     <main>   
       <section>
         <h2 className='ms-title'>Aliens Fighting Championship</h2>
+
         <div className='hero'>
             <img src={HeroImage} alt="" />
+        </div>
+
+        <div className='alienFront'>
+          <div className='d-flex justify-content-center'>
+             <img className='front' src={renderAlienFront()} alt="" />
+          </div>
+          <div className="base">PLAYER 2: {player2Choice}</div>
+        </div>
+              
+
+        <div className='alienBack' >
+          <div className='d-flex justify-content-center'>
+            <img className='back' src={renderAlienBack()} alt="" />
+          </div>
+          <div className="base">PLAYER 1: {player1Choice} </div>
         </div>
       </section>  
     </main>
