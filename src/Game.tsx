ca287fc450b75ffecbcdc763047fb8d4b90552f5
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-12-20 22:46:54 +0100

    Implemented attack damage logic and HP bar reduction

--- src/Game.tsx
+++ src/Game.tsx
@@ -7,7 +7,6 @@ import meteoraxFront from './assets/images/meteorax_front.jpg';
 import voidonBack from './assets/images/voidon_back.jpg';
 import voidonFront from './assets/images/voidon_front.jpg';
 import aliensJson from './assets/aliens.json';
-
 import './assets/styles/Game.scss'
 import { useSelector } from 'react-redux';
 import  { useState } from 'react';
@@ -16,15 +15,11 @@ function Game() {
   const player1Choice = useSelector((state) => state.players.player1)
   const player2Choice = useSelector((state) => state.players.player2)
   const aliens = aliensJson;
-  const hpPlayer1 = hpBar1()
-  const hpPlayer2 = hpBar2()
   const [widthBar1, setWidth1] = useState(500); 
   const [widthBar2, setWidth2] = useState(500); 
-
-  let [attackTurn,setAttack] = useState(player1Choice);
-
-  console.log(widthBar1)
-
+  const [attackTurn,setAttack] = useState(player1Choice);
+  const [selectedAttack, setSelectedAttack] = useState('');
+  
   const aliensAvatar = [
     {
       id: 1,
@@ -67,37 +62,31 @@ function Game() {
     }
   }
 
-  function hpBar1(){
-    let hp = 0; 
-    for(let i = 0; i < aliens.length; i++){
-      if(aliens[i].nome === player1Choice){
-       hp = aliens[i].hp
-      }         
-      }
-      return hp
-    }
+  function attack(nomeAttacco) {
+    let damage = 0;
+    setSelectedAttack(nomeAttacco);
 
-    function hpBar2(){
-      let hp = 0;     
-      for(let i = 0; i < aliens.length; i++){
-        if(aliens[i].nome === player2Choice){
-         hp = aliens[i].hp
-        }         
+    //controllo se l'attacco selezionato è uguale a uno degli attacchi del json,
+    //allora 'damage' diventa pari al danno corrispondente di quell'attacco
+    
+    aliens.forEach(alieno => {
+      for(let i = 0; i < alieno.attacchi.length; i++){
+        if(alieno.attacchi[i].nome === selectedAttack){
+          damage = alieno.attacchi[i].danno
         }
-        return hp
       }
+    })
 
-      function attack() {
-
-        setAttack((turn) => {
-          if (turn === player1Choice) {
-            return player2Choice; // Se il turno attuale è player1, passa a player2
-          } else {
-            return player1Choice; // Altrimenti torna a player1
-          }
-        });
-
-      }
+    // poi, in base al turno attuale, dopo ogni attacco
+    // riduco barra hp con il 'damage', che è pari all'attacco selezionato
+    if (attackTurn === player1Choice) {
+      setWidth2((prevWidth) => prevWidth - damage); 
+      setAttack(player2Choice); 
+    } else {
+      setWidth1((prevWidth) => prevWidth - damage); 
+      setAttack(player1Choice); 
+    }
+  }
 
   return (
     <main>   
@@ -132,7 +121,7 @@ function Game() {
         <div className='menu'>
           {aliens.map((alieno, index) => 
             alieno.attacchi.filter((attacco) => alieno.nome === attackTurn).map((attacco) => (      
-                <div onClick={attack} className='attack'>
+                <div onClick={() => attack(attacco.nome)} className='attack'> 
                   {attacco.nome}
                 </div>
               ))
